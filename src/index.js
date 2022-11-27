12e3c1c9b061b282962ae21779ffb6d4c9c6456a
MarmoW <115847012+MarmoW@users.noreply.github.com>
2022-11-27 13:22:27 -0300

    ajuste pequeno

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
-import ReactDOM from "react-dom";
+//import ReactDOM from "react-dom";
 import App from "./App.js";
+import { createRoot } from 'react-dom/client';
+const container = document.getElementById('app');
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+root.render(<App tab="home" />);
 
-
-ReactDOM.render(<App />, document.querySelector(".pagetoda"));
+//ReactDOM.render(<App />, document.querySelector(".pagetoda"));
